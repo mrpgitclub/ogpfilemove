9c9d8c9bee46b9297d6c0ed865a29812078c8e5e
Travis Martinez <travisesteban91@gmail.com>
2023-05-19 17:16:01 -0400

    -spring cleaning

--- import pyodbc as pydb.py
+++
@@ -1,9 +0,0 @@
-import pyodbc as pydb
-
-server = '10.100.1.96'
-db = 'Plattsburgh_Owner'
-username = 'tmartinez'
-pwd = 'tenoneninenine1!'
-
-cnxn = pydb.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+db+';ENCRYPT=yes;UID='+username+';PWD='+ pwd)
-cursor = cnxn.cursor()
