57f5e361bae569d9d57ca4b26a2466de14f0e2ac
Travis Martinez <travisesteban91@gmail.com>
2023-05-19 17:09:56 -0400

    - finalized workflow in main - 0.95 repeating - fixed watchdog, but need to double check proper functionality - proper naming specific product codes are working

--- OGP Interface.py
+++ OGP Interface.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pandas as pd
 import tkinter as tk
+from tkinter import simpledialog
 import tkinter.ttk as ttk
 import sqlite3
 import time
@@ -26,15 +27,20 @@ def dataVerify(dataframe,trackerdata):
 
     return dataframe
 
-def submitshots(dfObject, filename):
-    dfObject.to_csv(str(outputDir + '\\' + filename), header = False, index = False)
-    wdEventHandler.uploadDispatchState = True
+def submitshots(dfObject, filename, opCode):
+    if opCode != 2: dfObject.to_csv(str(outputDir + '\\' + filename), header = False, index = False, date_format = '%m/%d/%Y %H:%M')
+    if opCode == 0: wdEventHandler.uploadDispatchState = True
+    if opCode == 2: 
+        writer = pd.ExcelWriter('Project Tool.xlsm', mode = 'a')
+        dfObject.to_excel(writer, sheet_name = 'Data', header = True, startrow = 2, startcol = 1, engine = 'openpyxl')
 
     return
 
 def grabData(crsr, tableList, tableIndex = 0): #use twoPartProgIndicator to fetch the first table or the second table
-    dfObject = pd.read_sql_query(f'SELECT * FROM {tableList[tableIndex]}', crsr)
+    dfObject = pd.read_sql_query(f'SELECT * FROM "{tableList[tableIndex]}"', crsr)
     if dfObject.size > 0: dfObject.columns = [column.replace(" ", "_") for column in dfObject.columns] #replace spaces with underscores for formatting
+    dfObject = dfObject.applymap(lambda x: x.strip() if isinstance(x, str) else x) #strip whitespace in a dataframe
+    dfObject = dfObject.applymap(lambda x: round(x, 4) if isinstance(x, float) else x) #strip whitespace in a dataframe
 
     return dfObject
 
@@ -49,7 +55,10 @@ def formatQCtoDF(dataframe):
 
     dataframe.pop('Fails') #delete fails column
     dataframe.insert(len(dataframe.columns) - 1,'Date_Time', dataframe.pop('Date_Time')) # Replaces the date time column to the correct location
-
+    dataframe["Date_Time"] = pd.to_datetime(dataframe["Date_Time"], format = 'ISO8601')
+    #print(dataframe["Date_Time"])
+    #date_format = 'YYYY/MM/DD HH:MM:SS'
+    # %Y/%m%d %H:%M:%S
     return dataframe
 
 def mergeTwoDataframes(dfObject, second_dfObject, partnoSql):
@@ -62,9 +71,9 @@ def mergeTwoDataframes(dfObject, second_dfObject, partnoSql):
     return dfObject
 
 def twoPartCRC(dfPartone,dfParttwo): #to execute when the part number correlates to a two part crc inner program 
-    dfPartone.insert(6,'TOP_OD_DIA',dfParttwo.pop(dfPartone.columns[0], axis = 1))   #swapped to index call- testing needed
-    dfPartone.insert(7,'HUL_ZD',dfParttwo.pop(dfPartone.columns[0], axis = 1))
-    dfPartone.insert(8,'Weight_RES',dfParttwo.pop(dfPartone.columns[0], axis = 1))
+    dfPartone.insert(6,'TOP_OD_DIA',dfParttwo.pop(dfParttwo.columns[0]))   #swapped to index call- testing needed
+    dfPartone.insert(7,'HUL_ZD',dfParttwo.pop(dfParttwo.columns[0]))
+    dfPartone.insert(8,'Weight_RES',dfParttwo.pop(dfParttwo.columns[0]))
     return dfPartone
 
 def twoPartOllyOuter(dfPartone,dfParttwo):
@@ -82,27 +91,34 @@ def twoPartOllyInner(dfPartone,dfParttwo):
     return dfPartone
 
 def checkPartno(part, conn):
-    sql = """SELECT Part_number, Part_Type FROM Part_Numbers2 WHERE Part_number = ?""" #provides SQL queury statement with option for parameter
-    confirmedPartType = False
+    sql = "SELECT Part_Type FROM Part_Numbers2 WHERE Part_number = ?" #provides SQL queury statement with option for parameter
+    confirmedPartExists = False
+    partnoSql = None
 
-    while confirmedPartType is False:
-        partDB = pd.read_sql_query(sql, conn,params=[part])  #fetchs the line item in the DB file matching the part
-        if partDB["Part_number"].loc[0] == part: confirmedPartType = True #extracts only the part type, to check for two part program
-        else: 
-            part = str(tk.simpledialog.askstring('OGP Interface', 'The Given part number is not recognized, please re-enter the part number:'))
+    #confirm part exists in the DB
+    while confirmedPartExists is False:
+        partDB = pd.read_sql_query("SELECT Part_number FROM Part_Numbers2 WHERE Part_number = ?", conn,params=[part])  #fetchs the line item in the DB file matching the part
+        if partDB.size == 0:
+            part = tk.simpledialog.askstring('OGP Interface', 'The Given part number is not recognized, please re-enter the part number:')
             if part is None: 
-                partnosql = False
+                partnoSql = False
                 break
-        partnosql = partDB["Part_Type"].loc[0] #extracts only the part type, to check for two part program
+        else: confirmedPartExists = True
+
+    #then fetch the part type
+    if partnoSql is not False:
+        partDB = pd.read_sql_query(sql, conn,params=[part])  #fetchs the line item in the DB file matching the part
+        if partDB.size == 0: partnoSql = None #extracts only the part type, to check for two part program
+        else: partnoSql = partDB["Part_Type"].loc[0] #extracts only the part type, to check for two part program
     
-    return partnosql
+    return partnoSql
 
 def grabfilenameData(location,workOrder):   #works
     trackerData = pd.read_excel(location, sheet_name = 'Production',dtype=str, engine = 'openpyxl')
     trackerData.columns = [column.replace(" ", "_") for column in trackerData.columns]
     trackerData.query("Work_Order == @workOrder", inplace=True)
     while trackerData.empty:
-        workOrder = str(tk.simpledialog.askinteger('Wrong Workorder', 'Unable to find WO#, try again'))
+        workOrder = tk.simpledialog.askinteger('Wrong Workorder', 'Unable to find WO#, try again')
         if workOrder is None:
             trackerData = None
             break
@@ -119,24 +135,28 @@ def namer(trackerData, conn):
         case 'Resin Specific':
             if trackerData['Product_Code'].iloc[0] == 'CI038' and trackerData['Material'].iloc[0] == 'CP0001': filename = str(str(trackerData['Work_Order'].iloc[0]) + ' ' + str(trackerData['Product_Code'].iloc[0]) + ' ' + str(trackerData['Cav'].iloc[0]) + 'cav ' + str(trackerData['Mold_#'].iloc[0]) + '.csv')
             else: filename = str(str(trackerData['Work_Order'].iloc[0]) + ' ' + str(trackerData['Product_Code'].iloc[0]) + resins[trackerData['Material'].iloc[0]] + ' ' + str(trackerData['Cav'].iloc[0]) + 'cav ' + str(trackerData['Mold_#'].iloc[0]) + '.csv') 
-        case 'Mold Specific':
-            filename = str(str(trackerData['Work_Order'].iloc[0]) + ' ' + str(trackerData['Product_Code'].iloc[0]) + '-mold-' + str(trackerData['Mold_#']) + ' ' + str(trackerData['Cav'].iloc[0]) + 'cav ' + str(trackerData['Mold_#'].iloc[0]) + '.csv')
+        case 'Mold specific':
+            filename = str(str(trackerData['Work_Order'].iloc[0]) + ' ' + str(trackerData['Product_Code'].iloc[0]) + '-mold-' + str(trackerData['Mold_#'].iloc[0]) + ' ' + str(trackerData['Cav'].iloc[0]) + 'cav ' + str(trackerData['Mold_#'].iloc[0]) + '.csv')
         case 'Customer Specific':
             filename = str(str(trackerData['Work_Order'].iloc[0]) + ' ' + str(trackerData['Product_Code'].iloc[0]) + ' ' + str(trackerData['Cav'].iloc[0]) + 'cav ' + str(trackerData['Mold_#'].iloc[0]) + '.csv')
         case _:
             filename = str(str(trackerData['Work_Order'].iloc[0]) + ' ' + str(trackerData['Product_Code'].iloc[0]) + ' ' + str(trackerData['Cav'].iloc[0]) + 'cav ' + str(trackerData['Mold_#'].iloc[0]) + '.csv')
     return filename
 
-def main(): 
+def main(opCode = 0): 
     dailyTracker ='G:\\SHARED\\QA\\SPC Daily Tracker\\2023 SPC Daily Tracker.xlsm'
+    twoPartProgramPartTypes = ['CRC Inner', 'Olly Outer', 'Olly Inner', 'dosage cup']
     conn_str = (
         r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
         r'DBQ=S:\\ogptest - Copy.mdb;'
         )
+    filename = f'rawdata {time.time()}.csv'
+    partnoSql = None
     conn = sqlite3.connect(str(file_path + '\\Part_Numbers2.db')) #small database of partnumbers for verification and checking for two part programs
     cnxn = pyodbc.connect(conn_str)
     crsr = cnxn.cursor()
 
+    #confirm files exist
     if not os.path.isfile(dailyTracker): return messagebox.showinfo(title = "OGP Interface", message = "Unable to locate SPC Daily Tracker")
     if not os.path.isfile(str(file_path + '\\Part_Numbers2.db')): return messagebox.showinfo(title = "OGP Interface", message = "Unable to connect to database: 0x0001")
     if not os.path.isfile(str('S:\\ogptest - Copy.mdb')): return messagebox.showinfo(title = "OGP Interface", message = "Unable to connect to database: 0x0002")
@@ -144,36 +164,45 @@ def main():
     tableList = list()
     for table_info in crsr.tables(tableType = 'TABLE'): tableList.append(table_info.table_name)
     
+    #fall through for each step in the workflow
     if len(tableList) < 1: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find measurements in the OGP: 0x0003")
 
     dfObject = grabData(cnxn, tableList)
     if dfObject.size == 0: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find measurements in the OGP: 0x0004")
 
-    partnoSql = checkPartno(dfObject.iloc[-1]["Product_Code"].strip(), conn)   #check for two part programs here
-    if partnoSql is False: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find measurements in the OGP: 0x0005")
+    if opCode == 0:
+        partnoSql = checkPartno(dfObject.iloc[-1]["Product_Code"].rstrip(), conn)   #check for two part programs here
+        if partnoSql is False: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find measurements in the OGP: 0x0005")
 
-    trackerData = grabfilenameData(dailyTracker, dfObject.iloc[-1]["Work_Order"].strip())
-    if trackerData is None: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find data in SPC Daily Tracker: 0x0006")
+        trackerData = grabfilenameData(dailyTracker, dfObject.iloc[-1]["Work_Order"].strip())
+        if trackerData is None: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find data in SPC Daily Tracker: 0x0006")
     
-    dfObject = formatQCtoDF(dfObject)
-    if dfObject.size == 0: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find measurements in the OGP: 0x0007")
+        dfObject = formatQCtoDF(dfObject)
+        if dfObject.size == 0: return messagebox.showinfo(title = "OGP Interface", message = "Unable to find measurements in the OGP: 0x0007")
 
-    if partnoSql is not None: 
-        second_dfObject = grabData(cnxn, tableList, 1)
-        dfObject = mergeTwoDataframes(dfObject, second_dfObject, partnoSql)
+        if partnoSql in twoPartProgramPartTypes: 
+            second_dfObject = grabData(cnxn, tableList, 1)
+            dfObject = mergeTwoDataframes(dfObject, second_dfObject, partnoSql)
 
-    filename = namer(trackerData, conn)
-    if filename is None: filename = str(tk.simpledialog.askstring('OGP Interface', 'Couldn\'t create a filename. Input a filename for the B drive: '))
+        filename = namer(trackerData, conn)
+        if filename is None: filename = tk.simpledialog.askstring('OGP Interface', 'Couldn\'t create a filename. Input a filename for the B drive: ')
     
-    submitshots(dfObject, filename)  #implement raw data export, cherry pick certain functions from main()
-    testWatchDog.start()                        #trim whitespace in submitshots()
-    while(wdEventHandler.uploadDispatchState is True):
+    submitshots(dfObject, filename, opCode)  #implement raw data export, cherry pick certain functions from main()
+
+    timeout = 0
+    while(wdEventHandler.uploadDispatchState is True and timeout < 10):
         time.sleep(1)
-    else:   #TEST THIS A LOT
-        crsr.execute(f'DROP TABLE {tableList[0]}')
-        if partnoSql is not None: crsr.execute(f'DROP TABLE {tableList[0]}')             #delete the two tables here
+        timeout += 1
+    else: 
+        if timeout < 10:
+            crsr.execute(f'DROP TABLE "{tableList.pop(0)}"') #TEST THIS A LOT
+            crsr.commit()
+            if opCode == 0 and partnoSql in twoPartProgramPartTypes:
+                crsr.execute(f'DROP TABLE "{tableList.pop(0)}"')             #delete the two tables here
+                crsr.commit()
 
-    testWatchDog.stop()
+        else: messagebox.showinfo(title = "OGP Interface", message = "Operation timed out. Try again.")
+
     conn.close()
     crsr.close()
     cnxn.close()
@@ -187,6 +216,7 @@ def main():
 
 mainGUI = tk.Tk()
 mainGUI.title("OGP Interface")
+mainGUI.attributes('-topmost', 'true')
 for num in range(1, 5): [mainGUI.columnconfigure(num, minsize = 15), mainGUI.rowconfigure(num, minsize = 15)]
 
 #maybe add a checkbox for validations? rather than production shots
@@ -195,7 +225,10 @@ tk.Frame(mainGUI).grid(column = 4, row = 1)
 tk.Frame(mainGUI).grid(column = 1, row = 4)
 tk.Frame(mainGUI).grid(column = 4, row = 4)
 
-tk.Button(mainGUI, text = "Submit Shot", command = main).grid(column = 2, row = 2)
+tk.Button(mainGUI, text = "Production", command = lambda: main(0)).grid(column = 2, row = 2)
+tk.Button(mainGUI, text = "Raw Data", command = lambda: main(1)).grid(column = 3, row = 2)
+tk.Button(mainGUI, text = "ESR Project", command = lambda: main(2)).grid(column = 2, row = 3)
+
 
 ###
 #   Classes
@@ -205,7 +238,8 @@ class ogpHandler(FileSystemEventHandler):
         self.uploadDispatchState = False    #indicates that a CSV file is being saved to the B drive for upload
         self.uploadSuccessState = False     #indicates that SFOL accepted or rejected the CSV file
     def on_modified(self, event):
-        self.uploadDispatchState = True
+        #self.uploadDispatchState = True
+        print(event.src_path)
         if event.src_path.find('backup') > -1:
             self.uploadSuccessState = True
             self.uploadDispatchState = False
@@ -218,11 +252,13 @@ class ogpHandler(FileSystemEventHandler):
 #   Global variables 
 ###
 
-outputDir = '\\\\lighthouse2020\\Data Import\\Production\\Testing'
+outputDir = '\\\\lighthouse2020\\Data Import\\Production\\Testing\\backup'
+#outputDir = '\\\\lighthouse2020\\Data Import\\Production\\Closures\\400 Style\\RM' #real world testing
 file_path = os.path.abspath(os.path.dirname(__file__))
 testWatchDog = Observer()
 wdEventHandler = ogpHandler()
 testWatchDog.schedule(wdEventHandler, path = outputDir, recursive = True)
+testWatchDog.start()
 resins = {'MRP-PP30-1':'PP','PS3101':'PS','CP0001':'CP','PPSR549M':'CP','HDPE 5618':'HD','PA68253 ULTRAMID':'-Nylon'}
 
 ###
